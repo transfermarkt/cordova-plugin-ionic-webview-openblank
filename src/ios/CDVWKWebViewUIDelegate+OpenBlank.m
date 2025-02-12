5a725c67113bf04f4726e86651c3636e6fda6848
GloriaTM <gloria.fuchs@transfermarkt.de>
2025-02-12 17:25:06 +0100

    Update CDVWKWebViewUIDelegate+OpenBlank.m

--- src/ios/CDVWKWebViewUIDelegate+OpenBlank.m
+++ src/ios/CDVWKWebViewUIDelegate+OpenBlank.m
@@ -11,21 +11,29 @@
 @implementation CDVWKWebViewUIDelegate (OpenBlank)
 - (WKWebView *)webView:(WKWebView *)webView createWebViewWithConfiguration:(WKWebViewConfiguration *)configuration forNavigationAction:(WKNavigationAction *)navigationAction windowFeatures:(WKWindowFeatures *)windowFeatures {
     if (!navigationAction.targetFrame.isMainFrame) {
-      NSURL *url = [[navigationAction request] URL];
-
-      // Debugausgabe der URL in der Konsole
-      NSLog(@"Öffne URL im externen Browser: %@", url.absoluteString);
-
-      UIApplication *application = [UIApplication sharedApplication];
-      if ([application canOpenURL:url]) {
-        if (@available(iOS 10.0, *)) {
-          [application openURL:url options:@{} completionHandler:nil];
-        } else {
-          [application openURL:url];
+        NSURL *url = navigationAction.request.URL;
+        
+        // Debugging: URL ausgeben
+        NSLog(@"Öffne URL im externen Browser: %@", url.absoluteString);
+        
+        UIApplication *application = [UIApplication sharedApplication];
+        if ([application canOpenURL:url]) {
+            if (@available(iOS 10.0, *)) {
+                [application openURL:url options:@{} completionHandler:^(BOOL success) {
+                    if (success) {
+                        NSLog(@"URL wurde im externen Browser geöffnet.");
+                    } else {
+                        NSLog(@"Fehler beim Öffnen der URL.");
+                    }
+                }];
+            } else {
+                [application openURL:url];
+            }
+            
+            // Explizites Stoppen der Navigation im WebView
+            [webView loadHTMLString:@"" baseURL:nil]; // Setzt den WebView-Inhalt auf leer
+            return nil;
         }
-        // Abbrechen der Navigation im WebView
-        return nil;
-      }
     }
     return nil;
 }
